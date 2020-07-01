01dac2db764c71e09250fed352616db37be62d58
Roja Murugesh <rojadharun@outlook.com>
2020-07-01 19:09:33 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
+import Form from './components/Form';
+import Login from "./components/login";
+import SignUp from "./components/signup";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Register from "./components/Register.js";
 
 function App() {
   return (
     <Router>
-   
-        
-          <Switch>
-            <Route exact path='/' component={Register} />
-            <Route path="/Register" component={Register} />
+    <div className="App">
+  <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/sign-in" component={Login} />
+            <Route path="/sign-up" component={SignUp} />
+          
+            <Route path="/Form" component={Form} />
           </Switch>
-       
-    </Router>
+      </div>
+  </Router>
   );
 }
 
